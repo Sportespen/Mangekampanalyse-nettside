9ec29cf45c859d1bb17c25bb5e1d917b3d727b1d
Sportespen <sportespen@gmail.com>
2026-08-06 17:42:25 +0200

    Oppdater nedlastingslenke til v7.2.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Sett inn offentlig nedlastingsadresse her når installasjonsfilen er publisert.
 // Eksempel: const DOWNLOAD_URL = "https://github.com/Sportespen/.../releases/latest/download/...exe";
-const DOWNLOAD_URL = "";
+const DOWNLOAD_URL ="https://github.com/Sportespen/Mangekampanalyse-Downloads/releases/download/v7.2.0/Mangekampanalyse_Pro_7.2.0_Setup.exe";
 
 const buttons = [
   document.getElementById("download-button"),
