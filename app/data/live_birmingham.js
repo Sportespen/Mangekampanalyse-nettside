1b3f08d94d7442479e8f744632c60b3e92e77a9c
Sportespen <sportespen@gmail.com>
2026-08-10 23:38:24 +0200

    Add live data snapshot scaffold

---
+++ app/data/live_birmingham.js
@@ -0,0 +1,8 @@
+window.MANGEKAMP_LIVE={
+  competition:'EM Birmingham 2026',
+  source:'https://live.european-athletics.com/birmingham-test-2026',
+  updatedAt:null,
+  status:'waiting',
+  men:{completedEvents:0,results:{}},
+  women:{completedEvents:0,results:{}}
+};
