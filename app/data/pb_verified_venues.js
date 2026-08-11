a09a8f8f60085f91adb2248e2615d00f8e8864e1
Sportespen <sportespen@gmail.com>
2026-08-11 11:35:23 +0200

    Verify all Johannes Erm PB venues

--- app/data/pb_verified_venues.js
+++ app/data/pb_verified_venues.js
@@ -13,6 +13,18 @@
         "Stav":{venue:"Bislett Stadion, Oslo (NOR)",date:"28 JUN 2023"},
         "Spyd":{venue:"Friidrettsstadion, Jessheim (NOR)",date:"23 AUG 2025"},
         "1500m":{venue:"Leppävaaran Stadion, Espoo (FIN)",date:"JUL 2023"}
+      },
+      "Johannes Erm":{
+        "100m":{venue:"Stadio Olimpico, Roma (ITA)",date:"10 JUN 2024"},
+        "Lengde":{venue:"Hayward Field, Eugene, OR (USA)",date:"06 JUN 2018"},
+        "Kule":{venue:"Emirates Arena, Glasgow (GBR) (i)",date:"02 MAR 2024"},
+        "Høyde":{venue:"Stade de France, Paris (FRA)",date:"02 AUG 2024"},
+        "400m":{venue:"Stadio Olimpico, Roma (ITA)",date:"10 JUN 2024"},
+        "110mh":{venue:"Athens, GA (USA)",date:"05 APR 2019"},
+        "Diskos":{venue:"Kadrioru staadion, Tallinn (EST)",date:"09 AUG 2020"},
+        "Stav":{venue:"Stade Pierre Paul Bernard, Talence (FRA)",date:"15 SEP 2024"},
+        "Spyd":{venue:"Stadio Olimpico, Roma (ITA)",date:"11 JUN 2024"},
+        "1500m":{venue:"Stade de France, Paris (FRA)",date:"03 AUG 2024"}
       }
     },
     women:{}
