a2b75b18f4f4bfdc2c7310416d89913b36c7712b
Sportespen <sportespen@gmail.com>
2026-08-13 01:31:23 +0200

    Add verified WA history for Roosen Tilga Jarvinen

---
+++ app/data/history_batch_sven_karel_tomas.js
@@ -0,0 +1,40 @@
+(function(){
+  const H=window.MANGEKAMP_HISTORY=window.MANGEKAMP_HISTORY||{};
+  const set=(name,series)=>{H[name]=Object.assign({},H[name]||{},series);};
+  set('Sven Roosen',{
+    '100m':[[10.52,'10.52','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Lengde':[[7.12,'7.12','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Kule':[[14.69,'14.69','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Høyde':[[1.88,'1.88','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    '400m':[[47.52,'47.52','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    '110mh':[[14.08,'14.08','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Diskos':[[48.07,'48.07','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Stav':[[4.70,'4.70','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Spyd':[[65.07,'65.07','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    '1500m':[[263.31,'4:23.31','Mösle-Stadium, Götzis','2026','2026-05-31']]
+  });
+  set('Karel Tilga',{
+    '100m':[[10.95,'10.95','Mösle-Stadium, Götzis','2026','2026-05-30'],[10.91,'10.91','Mösle-Stadium, Götzis','2025','2025-05-31']],
+    'Lengde':[[7.39,'7.39','Mösle-Stadium, Götzis','2026','2026-05-30'],[7.53,'7.53','Mösle-Stadium, Götzis','2025','2025-05-31']],
+    'Kule':[[15.58,'15.58','Mösle-Stadium, Götzis','2026','2026-05-30'],[15.83,'15.83','Mösle-Stadium, Götzis','2025','2025-05-31']],
+    'Høyde':[[1.97,'1.97','Mösle-Stadium, Götzis','2026','2026-05-30'],[1.91,'1.91','Mösle-Stadium, Götzis','2025','2025-05-31']],
+    '400m':[[49.47,'49.47','Mösle-Stadium, Götzis','2026','2026-05-30'],[49.27,'49.27','Mösle-Stadium, Götzis','2025','2025-05-31']],
+    '110mh':[[14.59,'14.59','Mösle-Stadium, Götzis','2026','2026-05-31'],[14.89,'14.89','Mösle-Stadium, Götzis','2025','2025-06-01']],
+    'Diskos':[[50.44,'50.44','Mösle-Stadium, Götzis','2026','2026-05-31'],[51.47,'51.47','Mösle-Stadium, Götzis','2025','2025-06-01']],
+    'Stav':[[4.70,'4.70','Mösle-Stadium, Götzis','2026','2026-05-31'],[4.50,'4.50','Mösle-Stadium, Götzis','2025','2025-06-01']],
+    'Spyd':[[65.69,'65.69','Mösle-Stadium, Götzis','2026','2026-05-31'],[69.59,'69.59','Mösle-Stadium, Götzis','2025','2025-06-01']],
+    '1500m':[[266.17,'4:26.17','Mösle-Stadium, Götzis','2026','2026-05-31'],[267.29,'4:27.29','Mösle-Stadium, Götzis','2025','2025-06-01']]
+  });
+  set('Tomas Järvinen',{
+    '100m':[[10.61,'10.61','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Lengde':[[7.48,'7.48','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Kule':[[12.88,'12.88','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    'Høyde':[[2.09,'2.09','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    '400m':[[47.69,'47.69','Mösle-Stadium, Götzis','2026','2026-05-30']],
+    '110mh':[[14.18,'14.18','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Diskos':[[43.76,'43.76','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Stav':[[4.90,'4.90','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    'Spyd':[[62.20,'62.20','Mösle-Stadium, Götzis','2026','2026-05-31']],
+    '1500m':[[275.82,'4:35.82','Mösle-Stadium, Götzis','2026','2026-05-31']]
+  });
+})();
