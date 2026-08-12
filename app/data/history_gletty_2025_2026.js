ec22affbd9d498a9abb3abaa5bbdd81dfe8a55cb
Sportespen <sportespen@gmail.com>
2026-08-13 00:23:22 +0200

    Add Makenson Gletty 2025-2026 WA result history

---
+++ app/data/history_gletty_2025_2026.js
@@ -0,0 +1,16 @@
+(function(){
+  const H=window.MANGEKAMP_HISTORY=window.MANGEKAMP_HISTORY||{};
+  const g=H['Makenson Gletty']||{};
+  H['Makenson Gletty']=Object.assign({},g,{
+    '100m':[[10.65,'10.65','Stadionring, Ratingen','2026','2026-06-27'],[10.92,'10.92','National Stadium, Tokyo','2025','2025-09-20'],[10.87,'10.87','Stade Pierre Paul Bernard, Talence','2025','2025-08-02']],
+    'Lengde':[[7.27,'7.27','Stadionring, Ratingen','2026','2026-06-27'],[7.24,'7.24','Stade Pierre Paul Bernard, Talence','2025','2025-08-02']],
+    'Kule':[[16.22,'16.22','Stadionring, Ratingen','2026','2026-06-27'],[15.40,'15.40','Stade Pierre Paul Bernard, Talence','2025','2025-08-02']],
+    'Høyde':[[1.94,'1.94','Stadionring, Ratingen','2026','2026-06-27'],[1.96,'1.96','National Stadium, Tokyo','2025','2025-09-20'],[2.03,'2.03','Stade Pierre Paul Bernard, Talence','2025','2025-08-02']],
+    '400m':[[47.21,'47.21','Stadionring, Ratingen','2026','2026-06-27'],[50.49,'50.49','National Stadium, Tokyo','2025','2025-09-20'],[49.36,'49.36','Stade Pierre Paul Bernard, Talence','2025','2025-08-02']],
+    '110mh':[[13.84,'13.84','Stadionring, Ratingen','2026','2026-06-28'],[14.06,'14.06','National Stadium, Tokyo','2025','2025-09-21'],[14.21,'14.21','Stade Pierre Paul Bernard, Talence','2025','2025-08-03']],
+    'Diskos':[[42.49,'42.49','Stadionring, Ratingen','2026','2026-06-28'],[44.16,'44.16','Stade Pierre Paul Bernard, Talence','2025','2025-08-03']],
+    'Stav':[[4.80,'4.80','Stadionring, Ratingen','2026','2026-06-28'],[4.80,'4.80','Stade Pierre Paul Bernard, Talence','2025','2025-08-03']],
+    'Spyd':[[61.01,'61.01','Stadionring, Ratingen','2026','2026-06-28'],[62.89,'62.89','National Stadium, Tokyo','2025','2025-09-21'],[56.48,'56.48','Stade Pierre Paul Bernard, Talence','2025','2025-08-03']],
+    '1500m':[[266.76,'4:26.76','Stadionring, Ratingen','2026','2026-06-28'],[267.90,'4:27.90','National Stadium, Tokyo','2025','2025-09-21'],[265.33,'4:25.33','Stade Pierre Paul Bernard, Talence','2025','2025-08-03']]
+  });
+})();
